18df8c61e5d5e4b7c1e962930ef55c45330d9764
José Victor <josevqczt@gmail.com>
2021-12-26 22:59:42 -0300

    fix: Nav position

--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 export const NavStyles = styled.nav`
   position: fixed;
   width: 100%;
+  top: 50px;
   max-width: 1200px;
   z-index: 9999;
 
   .navbar {
     &--list {
-      margin-top: 50px;
       text-align: end;
 
       &--bars {
