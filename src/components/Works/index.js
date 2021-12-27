8c7d2e7f260f901a908772ef51584e201fa2f7aa
José Victor <josevqczt@gmail.com>
2021-12-27 19:47:02 -0300

    feat: add images and backgrounds

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -8,12 +8,30 @@ function Works() {
     <WorksStyles className="works">
       <HeaderSection title="WORKS" />
       <div className="works--row">
-        <div className="works--row--item__size-1 works--row--item"></div>
-        <div className="works--row--item__size-2 works--row--item"></div>
+        <div className="works--row--item--omega works--row--item works--row--item__size-1 mr-50">
+          <img
+            className="works--row--item--omega--image"
+            alt="Omega Movimento Luz Livre"
+            src="/images/works/omega.png"
+          />
+        </div>
+        <div className="works--row--item--netflix works--row--item works--row--item__size-2">
+          <img
+            className="works--row--item--omega--image"
+            alt="Almanque Tudum Netflix"
+            src="/images/works/netflix.png"
+          />
+        </div>
       </div>
-      <div className="works--row">
-        <div className="works--row--item__size-2 works--row--item"></div>
-        <div className="works--row--item__size-1 works--row--item"></div>
+      <div className="works--row mt-50">
+        <div className="works--row--item--spotify works--row--item works--row--item__size-2">
+          <img
+            className="works--row--item--omega--image"
+            alt="Spotify Top Podcasters"
+            src="/images/works/spotify.png"
+          />
+        </div>
+        <div className="works--row--item--other works--row--item works--row--item__size-1 ml-50"></div>
       </div>
     </WorksStyles>
   );
--- src/components/Works/styles.js
+++ src/components/Works/styles.js
@@ -7,15 +7,38 @@ export const WorksStyles = styled.section`
       flex-direction: row;
 
       &--item {
-        height: 50px;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 25px;
 
+        &--omega {
+          background-color: var(--rose);
+
+          &--image {
+            float: right;
+          }
+        }
+        &--netflix {
+          background-color: var(--blue);
+
+          &--image {
+            float: right;
+          }
+        }
+        &--spotify {
+          background-color: var(--yellow);
+
+          &--image {
+            float: left;
+          }
+        }
+        &--other {
+          background-color: var(--green);
+        }
         &__size-1 {
           flex: 1;
-          border: 1px solid red;
         }
         &__size-2 {
           flex: 2;
-          border: 1px solid blue;
         }
       }
     }
