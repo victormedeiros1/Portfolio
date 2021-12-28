07002a27052470e06200ff751fe1305ea484161c
José Victor <josevqczt@gmail.com>
2021-12-27 23:24:03 -0300

    feat: Works background, texts

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -9,6 +9,16 @@ function Works() {
       <HeaderSection title="WORKS" />
       <div className="works--row">
         <div className="works--row--item--omega works--row--item works--row--item__size-1 mr-50">
+          <div className="works--row--item--header">
+            <h4 className="works--row--item--header--title">
+              Movimento <br />
+              Luz Livre
+            </h4>
+            <h5 className="works--row--item--header--subtitle">
+              Omega <br />
+              2021
+            </h5>
+          </div>
           <img
             className="works--row--item--omega--image"
             alt="Omega Movimento Luz Livre"
@@ -16,6 +26,16 @@ function Works() {
           />
         </div>
         <div className="works--row--item--netflix works--row--item works--row--item__size-2">
+          <div className="works--row--item--header">
+            <h4 className="works--row--item--header--title">
+              Almanaque <br />
+              Tudum
+            </h4>
+            <h5 className="works--row--item--header--subtitle">
+              Netflix <br />
+              2021
+            </h5>
+          </div>
           <img
             className="works--row--item--omega--image"
             alt="Almanque Tudum Netflix"
@@ -25,13 +45,28 @@ function Works() {
       </div>
       <div className="works--row mt-50">
         <div className="works--row--item--spotify works--row--item works--row--item__size-2">
+          <div className="works--row--item--header">
+            <h4 className="works--row--item--header--title">
+              Top <br />
+              Podcasters
+            </h4>
+            <h5 className="works--row--item--header--subtitle">
+              Spotify <br />
+              2021
+            </h5>
+          </div>
           <img
             className="works--row--item--omega--image"
             alt="Spotify Top Podcasters"
             src="/images/works/spotify.png"
           />
         </div>
-        <div className="works--row--item--other works--row--item works--row--item__size-1 ml-50"></div>
+        <div className="works--row--item--other works--row--item works--row--item__size-1 ml-50">
+          <div className="works--row--item--header works--row--item--other--content">
+            <img alt="logo" width="64px" src="/images/logo.svg" />
+            <p className="works--row--item--header--title">COMING SOON</p>
+          </div>
+        </div>
       </div>
     </WorksStyles>
   );
--- src/components/Works/styles.js
+++ src/components/Works/styles.js
@@ -7,9 +7,29 @@ export const WorksStyles = styled.section`
       flex-direction: row;
 
       &--item {
+        position: relative;
         box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
         border-radius: 25px;
 
+        &--header {
+          position: absolute;
+          top: 50%;
+          transform: translateY(-50%);
+          margin-left: 30px;
+
+          &--title {
+            color: var(--white);
+            font-weight: 700;
+            font-size: 24px;
+            line-height: 36px;
+          }
+          &--subtitle {
+            color: var(--white);
+            font-size: 20px;
+            line-height: 28px;
+            font-weight: 300;
+          }
+        }
         &--omega {
           background-color: var(--rose);
 
@@ -33,6 +53,22 @@ export const WorksStyles = styled.section`
         }
         &--other {
           background-color: var(--green);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          &--content {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            margin-left: -20px;
+
+            p {
+              font-weight: 300;
+              margin-top: 20px;
+            }
+          }
         }
         &__size-1 {
           flex: 1;
