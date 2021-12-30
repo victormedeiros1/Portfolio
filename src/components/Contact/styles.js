632bcdcd9da6fa69d1d31de4939041b75b24b47b
José Victor <josevqczt@gmail.com>
2021-12-30 00:50:03 -0300

    refactor: delete css

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -3,13 +3,21 @@ import styled from "styled-components";
 export const ContactStyles = styled.footer`
   display: flex;
   flex-direction: row;
-  justify-content: center;
+
+  @media (min-width: 800px) {
+    justify-content: center;
+  }
 
   .contact {
     &--left {
-      margin-right: 100px;
-      padding-right: 50px;
-      border-right: 1px solid var(--lightgray);
+      border-right: none;
+
+      @media (min-width: 800px) {
+        margin-right: 100px;
+        padding-right: 50px;
+        border-right: 1px solid var(--lightgray);
+      }
+
       &--medias {
         &--list {
           list-style: none;
@@ -19,20 +27,35 @@ export const ContactStyles = styled.footer`
               color: var(--dark);
               font-family: "Poppins", sans-serif;
               font-weight: 700;
-              font-size: 40px;
+              font-size: 25px;
+
+              @media (min-width: 800px) {
+                font-size: 40px;
+              }
 
               &:hover {
                 color: var(--green);
               }
 
               &--icon {
-                width: 30px;
-                height: 30px;
-                margin-right: 20px;
+                width: 15px;
+                height: 15px;
+                margin-right: 10px;
+
+                @media (min-width: 800px) {
+                  width: 30px;
+                  height: 30px;
+                  margin-right: 20px;
+                }
               }
             }
           }
         }
+      }
+    }
+    &--right {
+      @media (max-width: 800px) {
+        display: none;
       }
     }
   }
