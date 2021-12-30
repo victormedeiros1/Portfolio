e2ffa8167544e4332ce1f62b04f1dbc315514662
José Victor <josevqczt@gmail.com>
2021-12-30 19:20:23 -0300

    refactor: new url

--- src/components/MetaTagsHead/index.js
+++ src/components/MetaTagsHead/index.js
@@ -12,7 +12,7 @@ function MetaTagsHead() {
         content="Hi, I’m José Victor. Nice to meet you! I started programming in 2017 and graduated as a Computer Technician at the IFRN. I want to be a great developer and I am every day studying and striving more and more to become one."
       />
       <meta property="og:title" content="José Victor - Front End Dev" />
-      <meta property="og:image" content="./images/ogImage.png" />
+      <meta property="og:image" content="/images/ogImage.png" />
       <meta name="author" content="José Victor" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <ReactTitle title="José Victor - Front End Dev" />
