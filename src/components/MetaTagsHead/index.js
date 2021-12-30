750c3ada792370bac729fee53c97f9c06ce112f2
José Victor <josevqczt@gmail.com>
2021-12-30 19:29:30 -0300

    refactor: all ogTags

--- src/components/MetaTagsHead/index.js
+++ src/components/MetaTagsHead/index.js
@@ -11,8 +11,26 @@ function MetaTagsHead() {
         name="description"
         content="Hi, I’m José Victor. Nice to meet you! I started programming in 2017 and graduated as a Computer Technician at the IFRN. I want to be a great developer and I am every day studying and striving more and more to become one."
       />
+
+      <meta property="og:url" content="https://www.josevictor.dev/" />
+      <meta property="og:type" content="website" />
       <meta property="og:title" content="José Victor - Front End Dev" />
+      <meta
+        property="og:description"
+        content="Hi, I’m José Victor. Nice to meet you!"
+      />
       <meta property="og:image" content="/images/ogImage.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="josevictor.dev" />
+      <meta property="twitter:url" content="https://www.josevictor.dev/" />
+      <meta name="twitter:title" content="José Victor - Front End Dev" />
+      <meta
+        name="twitter:description"
+        content="Hi, I’m José Victor. Nice to meet you!"
+      />
+      <meta name="twitter:image" content="/images/ogImage.png" />
+
       <meta name="author" content="José Victor" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <ReactTitle title="José Victor - Front End Dev" />
