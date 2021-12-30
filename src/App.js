59c336d95db1346d48050a67e547edff0064c4a8
José Victor <josevqczt@gmail.com>
2021-12-30 13:14:16 -0300

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./global/global.css";
 import "./global/theme.css";
-
 import Container from "./components/Container";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
