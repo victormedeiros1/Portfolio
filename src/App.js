1eac19d75e051be163e327b7d969b0295e77dedf
José Victor <josevqczt@gmail.com>
2022-01-02 22:07:57 -0300

    feat: create css utils and move margins

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./global/global.css";
 import "./global/theme.css";
+import "./global/utils.css";
 import Container from "./components/Container";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
