d686a82062339e3f9c0c93986d5b0c57204732b1
victormedeiros1 <josevqczt@gmail.com>
2022-01-03 22:52:57 -0300

    feat: navigation effect in Navbar when scroll to a section

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,6 +4,46 @@ import { useEffect } from "react";
 
 function Nav() {
   useEffect(() => {
+    const aboutMeDistance = document.querySelector("#about-me").offsetTop;
+    const worksDistance = document.querySelector("#works").offsetTop + 100;
+    const skillsDistance = document.querySelector("#skills").offsetTop;
+    const contactDistance = document.querySelector("#contact").offsetTop + 500;
+
+    const aboutMeNavItem = document.querySelector(
+      ".navbar--list--item--link--about-me"
+    );
+    const worksNavItem = document.querySelector(
+      ".navbar--list--item--link--works"
+    );
+    const skillsNavItem = document.querySelector(
+      ".navbar--list--item--link--skills"
+    );
+    const contactNavItem = document.querySelector(
+      ".navbar--list--item--link--contact"
+    );
+
+    document.addEventListener("scroll", () => {
+      const scrollDistance = window.scrollY;
+      console.log(`scrollDistance ${scrollDistance}`);
+      if (scrollDistance >= aboutMeDistance && scrollDistance < worksDistance)
+        aboutMeNavItem.style.color = "var(--green)";
+      else aboutMeNavItem.style.color = "var(--black)";
+
+      if (scrollDistance >= worksDistance && scrollDistance < skillsDistance)
+        worksNavItem.style.color = "var(--green)";
+      else worksNavItem.style.color = "var(--black)";
+
+      if (scrollDistance >= skillsDistance && scrollDistance < contactDistance)
+        skillsNavItem.style.color = "var(--green)";
+      else skillsNavItem.style.color = "var(--black)";
+
+      if (scrollDistance >= contactDistance)
+        contactNavItem.style.color = "var(--green)";
+      else contactNavItem.style.color = "var(--black)";
+    });
+  }, []);
+
+  useEffect(() => {
     gsap.fromTo(
       ".navbar",
       {
@@ -24,30 +64,42 @@ function Nav() {
           alt="bars-top"
         />
         <li className="navbar--list--item">
-          <a className="navbar--list--item--link" href="#about-me">
+          <a
+            className="navbar--list--item--link navbar--list--item--link--about-me"
+            href="#about-me"
+          >
             ABOUT ME
           </a>
         </li>
         <li className="navbar--list--item">
-          <a className="navbar--list--item--link" href="#works">
+          <a
+            className="navbar--list--item--link navbar--list--item--link--works"
+            href="#works"
+          >
             WORKS
           </a>
         </li>
         <li className="navbar--list--item">
           <a
-            className="navbar--list--item--link navbar--list--item--link__disabled"
+            className="navbar--list--item--link navbar--list--item--link--dark navbar--list--item--link__disabled"
             href="#"
           >
             DARK
           </a>
         </li>
         <li className="navbar--list--item">
-          <a className="navbar--list--item--link" href="#skills">
+          <a
+            className="navbar--list--item--link navbar--list--item--link--skills"
+            href="#skills"
+          >
             SKILLS
           </a>
         </li>
         <li className="navbar--list--item">
-          <a className="navbar--list--item--link" href="#contact">
+          <a
+            className="navbar--list--item--link navbar--list--item--link--contact"
+            href="#contact"
+          >
             CONTACT
           </a>
         </li>
