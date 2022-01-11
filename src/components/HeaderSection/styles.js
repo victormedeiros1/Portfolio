fa46e02e0273aa560917723cc64aa33176e834e1
victormedeiros1 <josevqczt@gmail.com>
2022-01-11 09:23:45 -0300

    fix: HeaderSeaction margin-left

--- src/components/HeaderSection/styles.js
+++ src/components/HeaderSection/styles.js
@@ -15,7 +15,7 @@ export const HeaderSectionStyles = styled.header`
       line-height: 38px;
       color: var(--green);
       margin-bottom: 10px;
-
+      margin-left: 5px;
       @media (min-width: 800px) {
         font-size: 80px;
         line-height: 62px;
