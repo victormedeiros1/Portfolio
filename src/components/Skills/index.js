60231afa0cedbd10ad5bef1859edb812c39b2d86
victormedeiros1 <josevqczt@gmail.com>
2022-01-18 21:27:52 -0300

    feat: add Skills animation

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -4,7 +4,12 @@ import { SkillsStyles } from "./styles";
 
 function Skills() {
   return (
-    <SkillsStyles id="skills" className="skills">
+    <SkillsStyles
+      id="skills"
+      className="skills"
+      data-aos="zoom-in"
+      data-aos-delay="300"
+    >
       <h3 className="skills--title">SKILLS</h3>
       <img
         className="skills--circle"
