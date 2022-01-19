5d70ea1e59df9383956b83127a1bc74e88de6e9f
victormedeiros1 <josevqczt@gmail.com>
2022-01-18 21:27:19 -0300

    feat: add AboutMe animation

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,28 +14,48 @@ function AboutMe() {
             src="./images/profile-picture.png"
           />
         </div>
-        <h4 className="about-me--texts--title">
+        <h4
+          className="about-me--texts--title"
+          data-aos="fade-down"
+          data-aos-delay="100"
+        >
           Hi, I’m José Victor. Nice to meet you!
         </h4>
-        <p className="about-me--texts--paragraph">
+        <p
+          className="about-me--texts--paragraph"
+          data-aos="fade-down"
+          data-aos-delay="200"
+        >
           I started programming in 2017 and graduated as a Computer Technician
           at the IFRN. I liked to program, it was an interesting challenge, but
           I took a real rush when I got to the web development part.
         </p>
-        <p className="about-me--texts--paragraph">
+        <p
+          className="about-me--texts--paragraph"
+          data-aos="fade-down"
+          data-aos-delay="300"
+        >
           When I got to know the front-end and saw the infinite sea of
           possibilities, I fell in love, I always liked the visual and creative
           side of things, so I started to interact with the community, working
           as a team, meeting and exchanging knowledge with new people.
           essential.
         </p>
-        <p className="about-me--texts--paragraph">
+        <p
+          className="about-me--texts--paragraph"
+          data-aos="fade-down"
+          data-aos-delay="400"
+        >
           Today, I'm studying Information Systems at UFRN. I want to be a great
           developer and I am every day studying and striving more and more to
           become one.
         </p>
       </div>
-      <div className="about-me--profile-picture__desktop">
+      <div
+        className="about-me--profile-picture__desktop"
+        data-aos="fade-right"
+        data-aos-delay="500"
+      >
         <img
           className="about-me--profile-picture__desktop--photo"
           alt="José Victor"
