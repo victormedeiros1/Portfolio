9cec891996454a115b4087ceaacb0ffe76527c4b
victormedeiros1 <josevqczt@gmail.com>
2022-01-18 21:02:07 -0300

    feat: add Main animation

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -7,12 +7,23 @@ function Main() {
     <MainStyles id="main" className="main">
       <header className="main--header">
         <div className="main--header--left">
-          <h1 className="main--header--left--h1">
+          <h1 className="main--header--left--h1" data-aos="fade-right">
             JOSÉ
             <br /> VICTOR
           </h1>
-          <h2 className="main--header--left--h2">FRONT END DEVELOPER</h2>
-          <a className="main--header--left--discover" href="#about-me">
+          <h2
+            className="main--header--left--h2"
+            data-aos="fade-right"
+            data-aos-delay="300"
+          >
+            FRONT END DEVELOPER
+          </h2>
+          <a
+            className="main--header--left--discover"
+            href="#about-me"
+            data-aos="fade-right"
+            data-aos-delay="600"
+          >
             DISCOVER
           </a>
         </div>
