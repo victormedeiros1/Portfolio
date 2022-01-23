9069b5e3a30005bb55136b3333cce700032ba4a2
victormedeiros1 <josevqczt@gmail.com>
2022-01-23 18:46:45 -0300

    refactor: all MetaTags

--- src/components/MetaTagsHead/index.js
+++ src/components/MetaTagsHead/index.js
@@ -4,13 +4,16 @@ import MetaTags, { ReactTitle } from "react-meta-tags";
 function MetaTagsHead() {
   return (
     <MetaTags>
-      <meta charSet="utf-8" />
+      {/* <!-- Primary Meta Tags --> */}
       <link rel="apple-touch-icon" href="./favicon.ico" />
       <link rel="icon" href="./favicon.ico" />
-
-      {/* <!-- Primary Meta Tags --> */}
+      <meta charSet="utf-8" />
       <meta name="title" content="José Victor - Front End Dev" />
-      <meta name="description" content="ddd" />
+      <meta
+        name="description"
+        content="Hi, I’m José Victor. Nice to meet you! I started programming in 2017 and graduated as a Computer Technician at the IFRN. I want to be a great developer and I am every day studying and striving more and more to become one."
+      />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
@@ -20,24 +23,18 @@ function MetaTagsHead() {
         property="og:description"
         content="Hi, I’m José Victor. Nice to meet you! I started programming in 2017 and graduated as a Computer Technician at the IFRN. I want to be a great developer and I am every day studying and striving more and more to become one."
       />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
-      <meta
-        property="og:image"
-        content="https://www.josevictor.dev/images/ogImage.png"
-      />
+      <meta property="og:image" content="https://i.imgur.com/e7E5psQ.png" />
 
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://www.josevictor.dev/" />
-      <meta property="twitter:title" content="José Victor - Front End Dev" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:url" content="https://www.josevictor.dev/" />
+      <meta name="twitter:title" content="José Victor - Front End Dev" />
+
       <meta
-        property="twitter:description"
+        name="twitter:description"
         content="Hi, I’m José Victor. Nice to meet you! I started programming in 2017 and graduated as a Computer Technician at the IFRN. I want to be a great developer and I am every day studying and striving more and more to become one."
       />
-      <meta
-        property="twitter:image"
-        content="https://www.josevictor.dev/images/ogImage.png"
-      />
+      <meta name="twitter:image" content="https://i.imgur.com/e7E5psQ.png" />
       <ReactTitle title="José Victor - Front End Dev" />
     </MetaTags>
   );
