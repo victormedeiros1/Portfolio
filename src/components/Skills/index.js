c2233decb1c4388455099deec954d7b7ea72b574
victormedeiros1 <josevqczt@gmail.com>
2022-03-13 12:58:46 -0300

    feat: components: Universe, Orbit, Skill and rotateRight animation

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 
-import { SkillsStyles } from "./styles";
+import { SkillsStyles, Orbit, Skill, Universe } from "./styles";
 import i18next from "../../translates/i18next";
 
 function Skills() {
   return (
-    <div></div>
+    <SkillsStyles id="skills" className="skills">
+      <Universe>
+        <h1 class="skills__title">{i18next.t("skills.title")}</h1>
+        <Orbit size={400}>
+          <Skill src="/images/skills/html.svg"></Skill>
+        </Orbit>
+        <Orbit size={800}>
+          <Skill src="/images/skills/css.svg"></Skill>
+        </Orbit>
+        <Orbit size={1200}>
+          <Skill src="/images/skills/javascript.svg"></Skill>
+        </Orbit>
+        <Orbit size={1600}>
+          <Skill src="/images/skills/react.svg"></Skill>
+        </Orbit>
+        <Orbit size={2000}>
+          <Skill src="/images/skills/git.svg"></Skill>
+        </Orbit>
+        <Orbit size={2400}>
+          <Skill src="/images/skills/figma.svg"></Skill>
+        </Orbit>
+        <Orbit size={2800}>
+          <Skill src="/images/skills/bootstrap.svg"></Skill>
+        </Orbit>
+      </Universe>
+    </SkillsStyles>
+
     // Old COmponent
     // <SkillsStyles
     //   id="skills"
--- src/components/Skills/styles.js
+++ src/components/Skills/styles.js
@@ -1,6 +1,35 @@
 import styled from "styled-components";
 
 export const SkillsStyles = styled.section`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+
+  &__title {
+    position: absolute;
+    color: var(--green);
+    font-family: "Concert One", sans-serif;
+    font-size: 40px;
+    line-height: 26px;
+    margin-left: 12px;
+
+    @media (min-width: 500px) {
+      font-size: 48px;
+      line-height: 34px;
+    }
+
+    @media (min-width: 800px) {
+      font-size: 54px;
+      line-height: 41px;
+      margin-left: 30px;
+    }
+  }
+`;
+
+export const Universe = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -9,39 +38,27 @@ export const SkillsStyles = styled.section`
     height: 100vh;
     min-height: 720px;
   }
+`;
 
-  .skills {
-    &--title {
-      position: absolute;
-      color: var(--green);
-      font-family: "Concert One", sans-serif;
-      font-size: 40px;
-      line-height: 26px;
-      margin-left: 12px;
+export const Orbit = styled.div`
+  position: absolute;
+  border: 2px solid red;
+  border-radius: 50%;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
 
-      @media (min-width: 500px) {
-        font-size: 48px;
-        line-height: 34px;
-      }
+  animation: rotateRight 10s infinite linear;
 
-      @media (min-width: 800px) {
-        font-size: 54px;
-        line-height: 41px;
-        margin-left: 30px;
-      }
-    }
-    &--circle {
-      width: 100%;
-
-      @media (min-width: 800px) {
-        width: 50%;
-        max-width: 700px;
-      }
-
-      @media (min-width: 1440px) {
-        width: 100%;
-        max-width: 900px;
-      }
+  @keyframes rotateRight {
+    100% {
+      transform: rotate(360deg);
     }
   }
+`;
+
+export const Skill = styled.img`
+  position: absolute;
+  top: -50px;
+  left: calc(50% - 50px);
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.08);
 `;
