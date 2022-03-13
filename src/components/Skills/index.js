43337e3dc1fb2df214989588d3a25cf2829fe942
victormedeiros1 <josevqczt@gmail.com>
2022-03-12 21:19:25 -0300

    feat: Skills individual svg's

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,19 +5,21 @@ import i18next from "../../translates/i18next";
 
 function Skills() {
   return (
-    <SkillsStyles
-      id="skills"
-      className="skills"
-      data-aos="zoom-in"
-      data-aos-delay="300"
-    >
-      <h3 className="skills--title">{i18next.t("skills.title")}</h3>
-      <img
-        className="skills--circle"
-        alt="Skills Circle"
-        src="./images/skills.svg"
-      />
-    </SkillsStyles>
+    <div></div>
+    // Old COmponent
+    // <SkillsStyles
+    //   id="skills"
+    //   className="skills"
+    //   data-aos="zoom-in"
+    //   data-aos-delay="300"
+    // >
+    //   <h3 className="skills--title">{i18next.t("skills.title")}</h3>
+    //   <img
+    //     className="skills--circle"
+    //     alt="Skills Circle"
+    //     src="./images/skills.svg"
+    //   />
+    // </SkillsStyles>
   );
 }
 
